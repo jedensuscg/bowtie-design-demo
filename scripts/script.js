79ddf741ddd3a0976c39d472344f6f29823dba23
James Edens <jedensuscg@gmail.com>
2020-10-15 18:07:57 -0400

    Added carousel for styles. Conntected "company" dropdown links.

--- scripts/script.js
+++ scripts/script.js
@@ -10,11 +10,29 @@ $(document).ready(function(){
         $("#who-btn").addClass("btn-selected"),
         $("#what-btn, #believe-btn").removeClass("btn-selected"),
         $("#what, #believe").slideUp("slow")
-    })
+    });
     $("#believe-btn").click(function(){
         $("#believe").slideDown("slow"),
         $("#believe-btn").addClass("btn-selected"),
         $("#what-btn, #who-btn").removeClass("btn-selected"),
         $("#what, #who").slideUp("slow")
-    })
+    });
+    $("#dropdown-who").click(function(){
+        $("#who").show(),
+        $("#who-btn").addClass("btn-selected"),
+        $("#what-btn, #believe-btn").removeClass("btn-selected"),
+        $("#what, #believe").hide()
+    });
+    $("#dropdown-what").click(function(){
+        $("#what").show(),
+        $("#what-btn").addClass("btn-selected"),
+        $("#who-btn, #believe-btn").removeClass("btn-selected"),
+        $("#who, #believe").hide()
+    });
+    $("#dropdown-believe").click(function(){
+        $("#believe").show(),
+        $("#believe-btn").addClass("btn-selected"),
+        $("#what-btn, #who-btn").removeClass("btn-selected"),
+        $("#what, #who").hide()
+    });
   });
